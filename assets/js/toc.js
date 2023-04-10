abb5943c5cd51a2cf6100ecea6c0d4a979e6b7f8
kmules1 <50674690+kmules1@users.noreply.github.com>
2023-04-10 11:12:48 -0400

    Update toc.js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -7,7 +7,7 @@
       minimumHeaders: 3,
       headers: 'h1, h2, h3, h4, h5, h6',
       listType: 'ul', // values: [ol|ul]
-      showEffect: 'show', // values: [show|slideDown|fadeIn|none]
+      showEffect: 'none', // values: [show|slideDown|fadeIn|none]
       showSpeed: '0', // set to 0 to deactivate effect
       classes: { list: '',
                  item: '',
