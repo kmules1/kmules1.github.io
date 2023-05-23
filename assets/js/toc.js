fa6a0fd1c5190478f266c96d906361a47541934d
kmules1 <50674690+kmules1@users.noreply.github.com>
2023-05-23 12:54:37 -0400

    Update toc.js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -98,7 +98,7 @@
     
     //adding a link to top in each targetted header
     headers.each(function() {
-        $(this).append('<a href="#" class="to-top"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/></svg></a>');
+        $(this).append('<a href="#" class="to-top"><svg xmlns="http://www.w3.org/2000/svg" width="1ch" height="1ch" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/></svg></a>');
     });
   };
 })(jQuery);
