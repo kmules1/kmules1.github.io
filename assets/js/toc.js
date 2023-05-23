754653ef6704d1eb2c232e29f9487d1d8a2d97cb
kmules1 <50674690+kmules1@users.noreply.github.com>
2023-05-23 12:41:09 -0400

    Update toc.js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -67,7 +67,7 @@
     .each(function(_, header) {
       this_level = get_level(header);
       if (!settings.noBackToTopLinks && this_level === highest_level) {
-        $(header).addClass('top-level-header').after(return_to_top);
+        //$(header).addClass('top-level-header').after(return_to_top);
       }
       if (this_level === level) // same level as before; same indenting
         html += "<li class=\"" + settings.classes.item + "\">" + createLink(header);
@@ -98,7 +98,7 @@
     
     //adding a link to top in each targetted header
     headers.each(function() {
-        $(this).append('<a href="#" class="to-top">top</a>');
+        $(this).append('<a href="#" class="to-top"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/></svg></a>');
     });
   };
 })(jQuery);
